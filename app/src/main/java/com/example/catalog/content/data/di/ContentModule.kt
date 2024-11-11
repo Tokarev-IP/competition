0f5543fa32796b7a6e8a2614fa291f8045b6466b
TokarevIP <tokarevrab@gmail.com>
2024-11-11 13:01:02 +0000

    add new imports

--- app/src/main/java/com/example/catalog/content/data/di/ContentModule.kt
+++ app/src/main/java/com/example/catalog/content/data/di/ContentModule.kt
@@ -1,18 +1,18 @@
 package com.example.catalog.content.data.di
 
-import com.example.catalog.content.data.interfaces.FirebaseStorageDeleteInterface
-import com.example.catalog.content.data.interfaces.FirebaseStorageDownloadInterface
-import com.example.catalog.content.data.interfaces.FirebaseStorageUploadInterface
-import com.example.catalog.content.data.interfaces.FirestoreDeleteInterface
-import com.example.catalog.content.data.interfaces.FirestoreDownloadInterface
-import com.example.catalog.content.data.interfaces.FirestoreUploadInterface
-import com.example.catalog.content.data.interfaces.GeminiAiInterface
+import com.example.catalog.content.data.repositories.FirebaseStorageDeleteInterface
 import com.example.catalog.content.data.repositories.FirebaseStorageDeleteRepository
+import com.example.catalog.content.data.repositories.FirebaseStorageDownloadInterface
 import com.example.catalog.content.data.repositories.FirebaseStorageDownloadRepository
+import com.example.catalog.content.data.repositories.FirebaseStorageUploadInterface
 import com.example.catalog.content.data.repositories.FirebaseStorageUploadRepository
+import com.example.catalog.content.data.repositories.FirestoreDeleteInterface
 import com.example.catalog.content.data.repositories.FirestoreDeleteRepository
+import com.example.catalog.content.data.repositories.FirestoreDownloadInterface
 import com.example.catalog.content.data.repositories.FirestoreDownloadRepository
+import com.example.catalog.content.data.repositories.FirestoreUploadInterface
 import com.example.catalog.content.data.repositories.FirestoreUploadRepository
+import com.example.catalog.content.data.repositories.GeminiAiInterface
 import com.example.catalog.content.data.repositories.GeminiAiRepository
 import dagger.Binds
 import dagger.Module
