1cd412e55039f57ebbcce5012da99740177bf1a0
TokarevIP <tokarevrab@gmail.com>
2024-12-04 23:20:53 +0000

    add actions from topBar

--- app/src/main/java/com/example/catalog/content/presentation/screens/MenuScreen.kt
+++ app/src/main/java/com/example/catalog/content/presentation/screens/MenuScreen.kt
@@ -1,13 +1,25 @@
 package com.example.catalog.content.presentation.screens
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.MoreVert
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarData
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -18,6 +30,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import com.example.catalog.content.presentation.ContentUiEvents
 import com.example.catalog.content.presentation.ContentUiIntents
 import com.example.catalog.content.presentation.ContentUiStates
@@ -26,6 +40,7 @@ import com.example.catalog.content.presentation.common.GoBackNavigationButton
 import com.example.catalog.content.presentation.common.LoadingStateView
 import com.example.catalog.content.presentation.viewmodel.ContentViewModel
 import com.example.catalog.content.presentation.views.MenuView
+import com.example.catalog.content.presentation.views.dialogs.ChooseLanguageDialogView
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -39,6 +54,23 @@ internal fun MenuScreen(
 
     val snackBarHostState = remember { SnackbarHostState() }
     var snackBarMsg by remember { mutableStateOf<String?>(null) }
+
+    var isOpenedLanguageDialog by remember { mutableStateOf(false) }
+    var isMenuExpanded by remember { mutableStateOf(false) }
+    var selectedLanguage by remember { mutableStateOf<String?>(null) }
+
+    val chooseFolderLauncher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.OpenDocumentTree()) { uri: Uri? ->
+            uri?.let { notNullUri ->
+                contentViewModel.setUiEvent(
+                    ContentUiEvents.SaveMenuAsPdfFile(
+                        notNullUri,
+                        selectedLanguage
+                    )
+                )
+            } ?: run { snackBarMsg = "No folder was selected" }
+        }
+
 
     if (uiIntent is ContentUiIntents.ShowSnackBarMsg) {
         snackBarMsg = (uiIntent as ContentUiIntents.ShowSnackBarMsg).msg
@@ -63,6 +95,40 @@ internal fun MenuScreen(
                 navigationIcon = {
                     GoBackNavigationButton {
                         contentViewModel.setUiEvent(ContentUiEvents.GoBack)
+                    }
+                },
+                actions = {
+                    if (menuDishData.dishListAndSectionViewDataList.isNotEmpty()) {
+                        IconButton(
+                            onClick = { isMenuExpanded = !isMenuExpanded }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Outlined.MoreVert,
+                                contentDescription = "Open action menu"
+                            )
+                        }
+                        DropdownMenu(
+                            expanded = isMenuExpanded,
+                            onDismissRequest = { isMenuExpanded = false }
+                        ) {
+                            DropdownMenuItem(
+                                text = { Text("Save menu as PDF file") },
+                                onClick = {
+                                    isMenuExpanded = false
+                                    selectedLanguage = null
+                                    chooseFolderLauncher.launch(null)
+                                },
+                                enabled = (uiState is ContentUiStates.Show),
+                            )
+                            DropdownMenuItem(
+                                text = { Text("Save translated menu as PDF file") },
+                                onClick = {
+                                    isMenuExpanded = false
+                                    isOpenedLanguageDialog = true
+                                },
+                                enabled = (uiState is ContentUiStates.Show),
+                            )
+                        }
                     }
                 }
             )
@@ -96,4 +162,21 @@ internal fun MenuScreen(
 
     }
 
+    if (isOpenedLanguageDialog)
+        BasicAlertDialog(onDismissRequest = { isOpenedLanguageDialog = false }) {
+            Surface(
+                modifier = modifier.clip(RoundedCornerShape(16.dp))
+            ) {
+                ChooseLanguageDialogView(
+                    onCancel = { isOpenedLanguageDialog = false },
+                    onAccept = { language: String? ->
+                        isOpenedLanguageDialog = false
+                        if (language != null) {
+                            selectedLanguage = language
+                        }
+                        chooseFolderLauncher.launch(null)
+                    }
+                )
+            }
+        }
 }
