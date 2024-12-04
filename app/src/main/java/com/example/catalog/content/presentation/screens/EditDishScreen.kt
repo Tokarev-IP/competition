9ecee959c37053ea5e809951f704f477859972b6
TokarevIP <tokarevrab@gmail.com>
2024-12-04 23:21:40 +0000

    remove imports which don't use

--- app/src/main/java/com/example/catalog/content/presentation/screens/EditDishScreen.kt
+++ app/src/main/java/com/example/catalog/content/presentation/screens/EditDishScreen.kt
@@ -7,11 +7,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
